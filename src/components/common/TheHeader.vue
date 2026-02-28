32c128a590d981449bb213df393417a1cf978630
root <root@localhost.localdomain>
2026-02-28 11:37:33 +0000

    fix: change profile link to login link when user is not logged in
    - Change nav link from /profile to /login for unauthenticated users
    - Prevent navigation active state confusion when profile route redirects to login
    - Ensure consistent navigation behavior

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -22,7 +22,7 @@
               </div>
             </template>
             <template v-else>
-              <router-link to="/profile" class="nav-link nav-link--auth nav-link--primary">个人</router-link>
+              <router-link to="/login" class="nav-link nav-link--auth nav-link--primary">个人</router-link>
             </template>
           </div>
         </nav>
